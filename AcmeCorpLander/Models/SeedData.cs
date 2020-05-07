c3e7eefa6ad27f3ffbbce1306959893c77869db2
Søren Eriksen <soer33b0@edu.eal.dk>
2020-05-07 13:35:48 +0200

    seedData update

--- AcmeCorpLander/Models/SeedData.cs
+++ AcmeCorpLander/Models/SeedData.cs
@@ -20,48 +20,50 @@ namespace AcmeCorpLander.Models
                     return;   // DB has been seeded
                 }
 
-                context.Submission.AddRange(
+                for (int i = 0; i < 2; i++)
+                {
+                    context.Submission.AddRange(
                     new Submission
                     {
                         FullName = "Rasmus Paludan",
                         Email = "rp@stramkurs.dk",
-                        Age = 22,
-                        SerialNum = 58079554
+                        Age = 47,
+                        SerialNum = 95676611
                     },
                     new Submission
                     {
                         FullName = "Kaj Boyesen",
                         Email = "abe@hotmail.com",
                         Age = 45,
-                        SerialNum = 82514261
+                        SerialNum = 32427143
                     },
                     new Submission
                     {
                         FullName = "Søren Malling",
                         Email = "sm@hotmail.com",
                         Age = 55,
-                        SerialNum = 38875640
+                        SerialNum = 59395710
                     },
                     new Submission
                     {
                         FullName = "Gordon Ramsey",
                         Email = "gordon@idiotsandwich.com",
                         Age = 50,
-                        SerialNum = 46632564
+                        SerialNum = 74457705
                     },
                     new Submission
                     {
                         FullName = "Morgan Freeman",
                         Email = "MoFo@god.com",
                         Age = 100,
-                        SerialNum = 93172768
+                        SerialNum = 67800879
                     },
                     new Submission
                     {
                         FullName = "Kanye West",
                         Email = "kanye4pres@2020.com",
                         Age = 48,
-                        SerialNum = 88059526
+                        SerialNum = 65996900
                     },
                     new Submission
                     {
@@ -75,52 +77,46 @@ namespace AcmeCorpLander.Models
                         FullName = "Casper Christensen",
                         Email = "cc@klovn.dk",
                         Age = 52,
-                        SerialNum = 65996900
+                        SerialNum = 88059526
                     },
                     new Submission
                     {
                         FullName = "Ole Thestrup",
-                        Email = "hvaskød@duen.me",
+                        Email = "skaldu@munkesi.skideren",
                         Age = 80,
-                        SerialNum = 67800879
+                        SerialNum = 93172768
                     },
                     new Submission
                     {
                         FullName = "Kim Larsen",
                         Email = "kl@gasolin.dk",
                         Age = 63,
-                        SerialNum = 74457705
+                        SerialNum = 46632564
                     },
                     new Submission
                     {
                         FullName = "Margrethe II",
                         Email = "majestæten@kongehuset.dk",
                         Age = 80,
-                        SerialNum = 59395710
+                        SerialNum = 38875640
                     },
                     new Submission
                     {
                         FullName = "Mads Mikkelsen",
                         Email = "mm@casinoroyale.dk",
                         Age = 54,
-                        SerialNum = 32427143
+                        SerialNum = 82514261
                     },
                     new Submission
                     {
                         FullName = "Nikolaj-Lie Kaas",
                         Email = "eigil@groenslagter.dk",
                         Age = 46,
-                        SerialNum = 95676611
-                    },
-                    new Submission
-                    {
-                        FullName = "Peter Madsen",
-                        Email = "pm@ubåd.dk",
-                        Age = 49,
-                        SerialNum = 10057238
+                        SerialNum = 58079554
                     }
                 );
-                context.SaveChanges();
+                    context.SaveChanges();
+                }
             }
         }
     }
